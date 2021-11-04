025053e00d98189ebe0c6bb0b616b760a4c7f5d6
peterAnami <peter.andake@student.moringaschool.com>
2021-11-04 08:55:17 +0300

    admin site

--- awwards_app/admin.py
+++ awwards_app/admin.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Profile, Project, Rate
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Rate)
